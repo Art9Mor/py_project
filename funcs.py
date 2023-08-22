c211114c9fa3b06be382905e26256a9a9516a14a
Artem Moroz <deusestmaleficus@gmail.com>
2023-08-22 16:46:34 +0700

    add function for difference

--- funcs.py
+++ funcs.py
@@ -1,8 +1,18 @@
 def sum_nums(a, b):
     """
     Performs addition of two numbers
-    :param a:
-    :param b:
-    :return:
+    :param a: number 1
+    :param b: number 2
+    :return: summary of the numbers a and b
     """
     return a + b
+
+
+def diff_num(a, b):
+    """
+    Performs difference of two numbers
+    :param a: number 1
+    :param b: umber 2
+    :return: difference of the numbers a and b
+    """
+    return a - b
